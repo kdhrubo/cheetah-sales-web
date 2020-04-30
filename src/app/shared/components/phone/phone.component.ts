4419ce73de340d7c2fa3ba41b641a13097da9d3b
Dhrubo <dhrubo.kayal@gmail.com>
2020-04-30 01:27:30 -0500

    phone ui

--- src/app/shared/components/phone/phone.component.ts
+++ src/app/shared/components/phone/phone.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Phone } from 'src/app/models/phone.model';
+import { FormGroup } from '@angular/forms';
+import { FormlyFieldConfig, FormlyFormOptions } from '@ngx-formly/core';
+import { FormService } from '../../../services/form.service';
+import { PhoneService } from '../../../services/phone.service';
 
 @Component({
   selector: 'app-phone',
@@ -7,9 +12,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhoneComponent implements OnInit {
 
-  constructor() { }
+  rsql = 'deleted==false';
+  phones: Phone[];
+
+  model = {};
+  form = new FormGroup({});
+
+  fields: FormlyFieldConfig[];
+
+  options: FormlyFormOptions = {
+    formState: {
+      disabled: true,
+    },
+  };
+
+  @Input() related: string;
+  @Input() relatedId: string;
+
+  constructor(private formService: FormService, private phoneService: PhoneService) {}
 
   ngOnInit(): void {
+    this.rsql = `deleted==false&relatedEntity=${this.related}&relatedEntityId=${this.relatedId}`;
+
+    this.getFormConfig();
+    this.search(this.rsql);
+  }
+  search(sql: string) {
+    this.phoneService.search(sql ).subscribe(
+      data => {
+        this.phones = data;
+      },
+      error => console.log('Error - ' + error.message)
+    );
+  }
+
+  getFormConfig() {
+    this.formService.getFields('form-phone').subscribe(
+      (data) => {
+        this.fields = data;
+      },
+
+      (error) => {
+        console.log('Unable to retrieve form information');
+      }
+    );
+  }
+
+  onSubmit() {
+    let phone: Phone = this.model as Phone;
+    phone.relatedEntity = this.related;
+    phone.relatedEntityId = this.relatedId;
+    this.phoneService.save(phone).subscribe(
+      (data) => {
+        console.log('Link save success');
+        this.search(this.rsql);
+      },
+      (error) => {
+        console.log('Link save failure');
+      }
+    );
+
   }
 
 }
