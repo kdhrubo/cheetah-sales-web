c5f559df1ac2e8ff9a41653119154fd7853c8407
Rajiv Kumar <rajiv.kumar.102@gmail.com>
2020-06-01 02:39:05 +0530

    category implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,15 @@ import { AuthGuard } from './guard/auth.guard';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: 'auth', pathMatch: 'full'},
-  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)  }, 
-  { path: 'leads', loadChildren: () => import('./lead/lead.module').then(m => m.LeadModule) , canActivate: [AuthGuard]}, 
+  { path: '', redirectTo: 'auth', pathMatch: 'full' },
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: 'leads', loadChildren: () => import('./lead/lead.module').then(m => m.LeadModule), canActivate: [AuthGuard] },
   { path: 'deals', loadChildren: () => import('./deal/deal.module').then(m => m.DealModule) },
-  { path: 'contacts', loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule) , canActivate: [AuthGuard]},
-  { path: 'accounts', loadChildren: () => import('./account/account.module').then(m => m.AccountModule) , canActivate: [AuthGuard]},
-  { path: 'product', loadChildren: () => import('./product/product.module').then(m => m.ProductModule) },
-  { path: 'category', loadChildren: () => import('./category/category.module').then(m => m.CategoryModule) },
-  { path: 'cockpit', loadChildren: () => import('./tenant/tenant.module').then(m => m.TenantModule) , canActivate: [AuthGuard] },
+  { path: 'contacts', loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule), canActivate: [AuthGuard] },
+  { path: 'accounts', loadChildren: () => import('./account/account.module').then(m => m.AccountModule), canActivate: [AuthGuard] },
+  { path: 'products', loadChildren: () => import('./product/product.module').then(m => m.ProductModule) },
+  { path: 'categories', loadChildren: () => import('./category/category.module').then(m => m.CategoryModule) },
+  { path: 'cockpit', loadChildren: () => import('./tenant/tenant.module').then(m => m.TenantModule), canActivate: [AuthGuard] },
   { path: 'guest', loadChildren: () => import('./guest/guest.module').then(m => m.GuestModule) },
   { path: 'docs', loadChildren: () => import('./document/document.module').then(m => m.DocumentModule) },
 ];
