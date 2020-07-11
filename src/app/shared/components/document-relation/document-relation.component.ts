a94b3274278541c7be5593eeeb80aec458335dcc
Dhrubo <dhrubo.kayal@gmail.com>
2020-07-11 01:18:24 -0500

    fix

---
+++ src/app/shared/components/document-relation/document-relation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-document-relation',
+  templateUrl: './document-relation.component.html',
+  styleUrls: ['./document-relation.component.css']
+})
+export class DocumentRelationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
