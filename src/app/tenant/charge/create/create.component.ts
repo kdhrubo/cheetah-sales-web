a7b650dcb7cb91e8ce4d135059ea61f593dacf6a
Rajiv Kumar <rajiv.kumar.102@gmail.com>
2020-09-06 23:39:09 +0530

    Charge create layout

--- src/app/tenant/charge/create/create.component.ts
+++ src/app/tenant/charge/create/create.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ChargeService } from '../../../services/charge.service';
+import { Charge } from 'src/app/models/charge/charge.model';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+import { FormGroup } from '@angular/forms';
+import { FormlyFieldConfig } from '@ngx-formly/core';
+import { FormService } from '../../../services/form.service';
 
 @Component({
   selector: 'app-create',
@@ -7,9 +14,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  form = new FormGroup({});
+  model = {};
+  fields: FormlyFieldConfig[];
+
+  constructor(private chargeService: ChargeService, private formService: FormService, private toastr: ToastrService, private router: Router) { }
 
   ngOnInit(): void {
+    this.formService.getFields('Charge-form').subscribe(
+
+      data => {
+        this.fields = data;
+        console.log('Retrieved charge form information sucessfully.');
+      },
+      error => {
+        console.log('Unable to retrieve charge form information.');
+      }
+
+    );
+  }
+
+  onSubmit() {
+    const charge: Charge = this.model as Charge;
+    Object.keys(charge).forEach((key) => (charge[key] === null || charge[key] === '') && delete charge[key]);
+
+    this.chargeService.save(charge)
+      .subscribe(
+        data => {
+          this.toastr.success('Charge Saved Successfully.', '', {});
+          this.router.navigate(['/app/cockpit/charge/list', data?.id]);
+        },
+        error => {
+          this.toastr.error('Charge save failed.', error?.detail, {});
+        }
+      );
   }
 
 }
